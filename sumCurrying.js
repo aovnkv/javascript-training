f5b4730f2cc1eb9e47e365efca5ae0ce083a4273
Andrei Vaniukov <aovnkv@yahoo.com>
2019-10-29 17:37:01 +0300

    refactor: currying

--- sumCurrying.js
+++ sumCurrying.js
@@ -1,7 +1,7 @@
 /**
  * Сделайте так чтобы это выражение работало: add(a, b) === add(a)(b)
  */
-const sum = () => [].slice.call(arguments).reduce((a, b) => a + b);
+const sum = (a, b) => a + b;
 
 const curry = fn => {
   return function curried(...args) {
@@ -16,6 +16,4 @@ const curry = fn => {
 
 const add = curry(sum);
 
-// console.log(add(3, 5, 7) === add(3)(5)(7));
-
-console.log(sum(3, 4));
+console.log(add(3, 5, 7) === add(3)(5)(7));
